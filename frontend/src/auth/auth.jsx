a17b91528aa43153f415fdfac8d67b9a6347a777
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-05-14 11:28:32 -0300

    feat: add login page

--- frontend/src/auth/auth.jsx
+++ frontend/src/auth/auth.jsx
@@ -3,27 +3,54 @@ import axios from "axios";
 import './auth.css'
 class Auth extends Component {
 
+    constructor(props) {
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.state = {
+            email: '',
+            senha: ''
+        };
+    }
 
+
+
+    onSubmit(e) {
+        const { email, senha } = this.state
+
+        e.preventDefault();
+
+        axios.post('http://localhost:3003/api/login', { email, senha })
+            .then(resp => {
+                
+
+            })
+            .catch(err => {
+                console.log(err.message);
+
+
+            })
+    }
 
     render() {
+        const { email, senha } = this.state
 
         return (
             <div className="login-container">
                 <div className="login-box">
                     <h1>Bem vindo!</h1>
-                    <form action="#" className="form-login">
+                    <form onSubmit={this.onSubmit} className="form-login">
                         <div className="grupo-inputs">
                             <label htmlFor="email">Email: </label>
-                            <input type="email" name="email" placeholder="Digite seu e-mail" />
+                            <input type="email" name="email" value={email} placeholder="Digite seu e-mail" onChange={(e) => this.setState({ email: e.target.value })} required />
                         </div>
                         <div className="grupo-inputs">
                             <label htmlFor="senha">Senha: </label>
-                            <input type="password" name="senha" placeholder="Digite seu e-mail" />
+                            <input type="password" name="senha" value={senha} placeholder="Digite sua senha" onChange={(e) => this.setState({ senha: e.target.value })} required />
 
                         </div>
                         <div className="grupo-inputs">
 
-                            <button type="submit">Entrar</button>
+                            <button type="submit" >Entrar</button>
                             <button>Criar nova conta</button>
                         </div>
 
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "../template/header/header";
 import './app.css'
 import Routes from "./routes";
 
