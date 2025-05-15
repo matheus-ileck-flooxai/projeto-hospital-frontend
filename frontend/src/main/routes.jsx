bf571840874fbaff7546f252d161c8d46e2a8748
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-05-15 12:22:34 -0300

    feat: add routes to menu items

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -3,16 +3,19 @@ import { Router, Route, Redirect, hashHistory } from 'react-router'
 import Auth from '../auth/auth'
 import Hospital from "../hospital/hospital";
 import Vaga from "../hospital/vagas/vaga";
+import Usuarios from "../hospital/usuarios/usuarios";
+import Voluntarios from "../hospital/voluntarios/voluntarios";
+import Pontuacao from "../hospital/pontuacao/pontuacao";
 
 export default props => (
 
     <Router history={hashHistory}>
         <Route path='/auth' component={Auth} />
         <Route path='/hospital' component={Hospital}>
-            <Route path='usuarios' component={Vaga}/>
+            <Route path='usuarios' component={Usuarios}/>
             <Route path='vagas' component={Vaga}/>
-            <Route path='voluntarios' component={Vaga}/>
-            <Route path='pontuacao' component={Vaga}/>
+            <Route path='voluntarios' component={Voluntarios}/>
+            <Route path='pontuacao' component={Pontuacao}/>
         </Route>
 
         <Redirect from='*' to='/auth' />
--- frontend/src/template/common/menu/menu.jsx
+++ frontend/src/template/common/menu/menu.jsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router';
 export default props =>
 
     <ul className="sidebar-menu">
-        <li className="menu-item">Usuarios</li>
+        <li className="menu-item"><Link to="/Hospital/usuarios">Usuarios</Link></li>
         <li className="menu-item"><Link to="/Hospital/vagas">Vagas</Link></li>
-        <li className="menu-item">Voluntarios</li>
-        <li className="menu-item">Pontuação</li>
+        <li className="menu-item"><Link to="/Hospital/voluntarios">Voluntarios</Link></li>
+        <li className="menu-item"><Link to="/Hospital/pontuacao">Pontuação</Link></li>
     </ul>
