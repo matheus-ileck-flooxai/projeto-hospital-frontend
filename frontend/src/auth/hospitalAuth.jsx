e405a99aaa4f008872ff338f2042f5e89e02c4d5
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-05-26 13:16:47 -0300

    refactor: change form from hospital auth to user auth

--- frontend/src/auth/hospitalAuth.jsx
+++ frontend/src/auth/hospitalAuth.jsx
@@ -9,7 +9,6 @@ export default class Auth extends Component {
 
     constructor(props) {
         super(props);
-        this.login = this.login.bind(this);
         this.register = this.register.bind(this);
         this.state = {
             email: '',
@@ -17,7 +16,6 @@ export default class Auth extends Component {
             name: '',
             address: '',
             phone_number: '',
-            showForm: false
         };
     }
 
@@ -42,20 +40,7 @@ export default class Auth extends Component {
         }
     }
 
-    login(e) {
-        e.preventDefault();
-        const { email, password } = this.state;
 
-        const url = 'https://projeto-hospital-backend-production.up.railway.app/api/hospital/login';
-        axios.post(url, { email, password })
-            .then(resp => {
-                localStorage.setItem('token', resp.data.token)
-                hashHistory.push('/hospital')
-            })
-            .catch(err => {
-                ;
-            })
-    }
 
     register(e) {
         e.preventDefault();
@@ -67,6 +52,7 @@ export default class Auth extends Component {
         axios.post(url, newHospital)
             .then(resp => {
                 this.setState({ showForm: false });
+                  hashHistory.push('/user/auth')
             })
             .catch(err => {
                 ;
@@ -79,26 +65,7 @@ export default class Auth extends Component {
         return (
             <div className="auth-container">
                 <div className="login-box">
-                    <img src={Logo} alt="Logo" className="logo" />
-                    {!showForm ? (
-                        <form onSubmit={this.login} className="form-login">
-                            <p className="msg-inicial">Seja bem vindo!</p>
-
-                            <div className="grupo-inputs">
-                                <label htmlFor="email" className="label">Email:</label>
-                                <input type="email" name="email" value={email} placeholder="Digite seu e-mail" onChange={(e) => this.setState({ email: e.target.value })} required />
-                            </div>
-                            <div className="grupo-inputs">
-                                <label htmlFor="password" className="label">Senha:</label>
-                                <input type="password" name="password" value={password} placeholder="Digite sua senha" onChange={(e) => this.setState({ password: e.target.value })} required />
-                            </div>
-                            <div className="grupo-inputs button">
-                                <button type="submit" className="btn-submit">Entrar</button>
-                                <a onClick={() => this.setState({ showForm: true })}>Criar nova conta</a>
-                            </div>
-                        </form>
-                    ) : (
-                        <form onSubmit={this.register} className="form-register">
+                   <form onSubmit={this.register} className="form-register">
                             <div className="grupo-inputs">
                                 <label htmlFor="name" className="label">Nome:</label>
                                 <input type="text" name="name" value={name} placeholder="Nome do hospital" onChange={(e) => this.setState({ name: e.target.value })} required />
@@ -121,10 +88,10 @@ export default class Auth extends Component {
                             </div>
                             <div className="grupo-inputs button">
                                 <button type="submit" className="btn-submit">Cadastrar</button>
-                                <a onClick={() => this.setState({ showForm: false })}>Já possuo uma conta</a>
+                                <a href="/#/user/auth">Já possuo uma conta</a>
                             </div>
                         </form>
-                    )}
+                
                 </div>
             </div>
         )
